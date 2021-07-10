3fb659838c933d01527df775bd116a6ca11e5e87
NohEunTae <dmsxoeldk@naver.com>
2021-07-10 16:28:53 +0900

    [Fix] Modify the extension code to use boundaries

--- Sources/VisibilityTrackableCollectionView/VisibilityTrackableCollectionViewExtension.swift
+++ Sources/VisibilityTrackableCollectionView/VisibilityTrackableCollectionViewExtension.swift
@@ -1,5 +1,5 @@
 //
-//  UICollectionViewExtension.swift
+//  VisibilityTrackableCollectionViewExtension.swift
 //  
 //
 //  Created by NohEunTae on 2021/07/01.
@@ -8,7 +8,7 @@
 #if os(iOS)
 import UIKit
 
-extension UICollectionView {
+extension VisibilityTrackableCollectionView {
     
     private func findSuperVC(_ responder: UIResponder) -> UIViewController? {
         guard let next = responder.next else { return nil }
@@ -19,41 +19,48 @@ extension UICollectionView {
         
         return vc
     }
-
-    var fullyVisibleCells: [UICollectionViewCell] {
+    
+    private func fullyVisibleItems<T: UIView>(views: [T]) -> [T] {
+        if let boundary = boundaryManager.boundary,
+           let boundaryView = boundary.view {
+            return views.filter {
+                let viewRect = convert($0.frame, to: boundaryView)
+                return boundary.rect.contains(viewRect) && bounds.contains($0.frame)
+            }
+        }
+        
         let superVC = findSuperVC(self)
-        return visibleCells.filter {
+        return views.filter {
             guard let superview = superVC?.view else {
                 return bounds.contains($0.frame)
             }
             
-            let cellRect = convert($0.frame, to: superview)
-            return superview.bounds.contains(cellRect)
+            let viewRect = convert($0.frame, to: superview)
+            return superview.bounds.contains(viewRect) && bounds.contains($0.frame)
         }
+    }
+    
+    var fullyVisibleCells: [UICollectionViewCell] {
+        fullyVisibleItems(views: visibleCells)
     }
     
     var fullyVisibleCellIndexPaths: [IndexPath] {
         fullyVisibleCells
             .compactMap { indexPath(for: $0) }
     }
-
+    
     func fullyVisibleReusableViews(ofKind elementKind: String) -> [UICollectionReusableView] {
-        let superVC = findSuperVC(self)
-        return visibleSupplementaryViews(ofKind: elementKind)
-            .filter {
-                guard let superview = superVC?.view else {
-                    return bounds.contains($0.frame)
-                }
-                
-                let viewRect = convert($0.frame, to: superview)
-                return superview.bounds.contains(viewRect)
-            }
+        fullyVisibleItems(views: visibleSupplementaryViews(ofKind: elementKind))
     }
     
     func fullyVisibleReusableIndexPaths(ofKind elementKind: String) -> [IndexPath] {
         fullyVisibleReusableViews(ofKind: elementKind)
             .compactMap { indexPathForVisibleSupplementaryView(for:$0, ofKind: elementKind) }
     }
+        
+}
+
+extension UICollectionView {
     
     func indexPathForVisibleSupplementaryView(for view: UICollectionReusableView, ofKind elementKind: String) -> IndexPath? {
         let indexPaths = indexPathsForVisibleSupplementaryElements(ofKind: elementKind)
@@ -62,6 +69,6 @@ extension UICollectionView {
         
         return items.first { $0.reusableView == view }?.indexPath
     }
-
+    
 }
 #endif
