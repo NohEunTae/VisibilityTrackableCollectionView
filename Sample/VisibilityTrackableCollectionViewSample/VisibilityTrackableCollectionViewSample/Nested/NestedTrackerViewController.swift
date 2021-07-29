e3075587b2468a3aeb5c4de00eec942e6e878ded
NohEunTae <dmsxoeldk@naver.com>
2021-07-29 16:34:42 +0900

    Sample Code > add refresh seen data & sections

--- Sample/VisibilityTrackableCollectionViewSample/VisibilityTrackableCollectionViewSample/Nested/NestedTrackerViewController.swift
+++ Sample/VisibilityTrackableCollectionViewSample/VisibilityTrackableCollectionViewSample/Nested/NestedTrackerViewController.swift
@@ -20,14 +20,40 @@ final class NestedTrackerViewController: UIViewController {
         )
     }()
     
+    private let refreshSectionButton = UIButton()
+    private let refreshSeenDataButton = UIButton()
+    private lazy var buttonContainer = UIStackView(arrangedSubviews: [refreshSeenDataButton, refreshSectionButton])
+
     private let adapter = NestedCollectionViewApater()
+    
+    private let normal1Section = NestedCollectionViewApater.Section.normal1.rawValue
+    private let normal2Section = NestedCollectionViewApater.Section.normal2.rawValue
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setupView()
     }
+        
+    @objc
+    private func reloadButtonDidClicked(_ sender: UIButton) {
+        collectionView.refreshSeenData()
+    }
+
+    @objc
+    private func reloadSectionButtonDidClicked(_ sender: UIButton) {
+        collectionView.refreshSections([normal1Section, normal2Section])
+    }
+}
+
+// MARK:- UI
+private extension NestedTrackerViewController {
     
-    private func setupView() {
+    func setupView() {
+        setupCollectionView()
+        setupButtonContainer()
+    }
+    
+    func setupCollectionView() {
         view.addSubview(collectionView)
         collectionView.snp.makeConstraints {
             $0.edges.equalTo(view.safeAreaLayoutGuide)
@@ -36,7 +62,46 @@ final class NestedTrackerViewController: UIViewController {
         collectionView.setBoundary(.init(view: attachTargetView()))
     }
     
-    private func attachTargetView() -> UIView {
+    func setupButtonContainer() {
+        view.addSubview(buttonContainer)
+        buttonContainer.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.leading.trailing.equalToSuperview().priority(.low)
+            $0.bottom.equalTo(view.safeAreaLayoutGuide).inset(20)
+        }
+        
+        [refreshSeenDataButton, refreshSectionButton].forEach { button in
+            button.snp.makeConstraints {
+                $0.width.equalTo(170)
+            }
+        }
+        
+        buttonContainer.spacing = 10
+        buttonContainer.axis = .horizontal
+
+        setupRefreshSeenDataButton()
+        setupRefreshSectionButton()
+    }
+    
+    func setupRefreshSeenDataButton() {
+        refreshSeenDataButton.setTitle("refresh seen data", for: .normal)
+        refreshSeenDataButton.titleLabel?.font = .systemFont(ofSize: 14, weight: .bold)
+        refreshSeenDataButton.backgroundColor = .darkGray.withAlphaComponent(0.7)
+        refreshSeenDataButton.clipsToBounds = true
+        refreshSeenDataButton.layer.cornerRadius = 10
+        refreshSeenDataButton.addTarget(self, action: #selector(reloadButtonDidClicked(_:)), for: .touchUpInside)
+    }
+    
+    func setupRefreshSectionButton() {
+        refreshSectionButton.setTitle("refresh sections \(normal1Section), \(normal2Section)", for: .normal)
+        refreshSectionButton.titleLabel?.font = .systemFont(ofSize: 14, weight: .bold)
+        refreshSectionButton.backgroundColor = .darkGray.withAlphaComponent(0.7)
+        refreshSectionButton.clipsToBounds = true
+        refreshSectionButton.layer.cornerRadius = 10
+        refreshSectionButton.addTarget(self, action: #selector(reloadSectionButtonDidClicked(_:)), for: .touchUpInside)
+    }
+    
+    func attachTargetView() -> UIView {
         let targetView = UIView()
         view.addSubview(targetView)
         targetView.isUserInteractionEnabled = false
